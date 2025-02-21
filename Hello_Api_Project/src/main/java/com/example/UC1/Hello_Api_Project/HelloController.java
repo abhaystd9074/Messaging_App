009bb63e5d24aef04417bb27659d830557c7f450
abhaystd9074 <abhaygautam907457@gmail.com>
2025-02-21 14:06:02 +0530

    Abhay Gautam add. UC2_Name_Query

--- Hello_Api_Project/src/main/java/com/example/UC1/Hello_Api_Project/HelloController.java
+++ Hello_Api_Project/src/main/java/com/example/UC1/Hello_Api_Project/HelloController.java
@@ -1,15 +1,19 @@
 package com.example.UC1.Hello_Api_Project;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-
+   // uc1 api
     @GetMapping(value = {"/uc1", "/hello"})
     public String sayHello() {
         return "Hello, World!";
     }
+    //UC2 Request parameter
+    @GetMapping("/input")
+    public String sayHello(@RequestParam(defaultValue = "Mark") String name) {
+        return "Hello " + name + " from BridgeLabz!";
+    }
+
 }
 
